01c5a23c47fbe4fa948923f31b25f0165bb6dd8f
SerbCSharp <serbcsharp@mail.ru>
2025-01-27 16:40:49 +0300

    Fix

--- main.js
+++ main.js
@@ -3,10 +3,22 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100 );
+camera.position.z = 3;
+scene.add(camera);
+
 const cubeGeometry = new THREE.BoxGeometry(1,1,1);
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 scene.add(cube);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+document.body.append( renderer.domElement );
+
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true;
 
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -23,18 +35,6 @@ window.addEventListener('dblclick', () => {
         document.exitFullscreen();
     }
 })
-
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100 );
-camera.position.z = 3;
-scene.add(camera);
-
-const orbitControls = new OrbitControls(camera, renderer.domElement);
-orbitControls.enableDamping = true;
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-document.body.append( renderer.domElement );
 
 const clock = new THREE.Clock();
 
