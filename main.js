19650843a48a240e3f106c71d43c79f7b65302c6
SerbCSharp <serbcsharp@mail.ru>
2025-01-28 15:41:01 +0300

    use image

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 import * as THREE from 'three';
-import gsap from 'gsap';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import GUI from 'lil-gui'
 
-const gui = new GUI({
-    width: 300,
-    title: 'Nice debug UI'
-});
-//gui.close();
-gui.hide();
-window.addEventListener('keydown', (event) => {
-    if(event.key == 'h')
-        gui.show(gui._hidden);
-})
-
-const debugObject = {};
+const image = new Image();
+image.src = '/static/textures/Wood_024/Wood_024_basecolor.jpg';
+const texture = new THREE.Texture(image);
+image.onload = () => {
+    texture.needsUpdate = true;
+};
 
 const scene = new THREE.Scene();
 
@@ -22,31 +14,10 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 camera.position.z = 3;
 scene.add(camera);
 
-debugObject.color = '#3a6ea6'
-
 const cubeGeometry = new THREE.BoxGeometry(1,1,1,2,2,2);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: debugObject.color, wireframe: true });
+const cubeMaterial = new THREE.MeshBasicMaterial({ map: texture });
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 scene.add(cube);
-
-const cubeTweaks = gui.addFolder('Awesome cube');
-cubeTweaks.add(cube.position, 'y').min(-3).max(3).step(0.01).name('elevation');
-cubeTweaks.add(cube, 'visible');
-cubeTweaks.add(cubeMaterial, 'wireframe');
-cubeTweaks.addColor(debugObject, 'color').onChange(() => {
-    cubeMaterial.color.set(debugObject.color)
-});
-debugObject.spin = () => {
-    gsap.to(cube.rotation, { y: cube.rotation.y + Math.PI * 2 })
-};
-cubeTweaks.add(debugObject, 'spin');
-debugObject.subdivision = 2;
-cubeTweaks.add(debugObject, 'subdivision').min(1).max(20).step(1).onFinishChange(() => {
-    cube.geometry.dispose();
-    cube.geometry = new THREE.BoxGeometry(
-        1, 1, 1,
-        debugObject.subdivision, debugObject.subdivision, debugObject.subdivision);
-});
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
